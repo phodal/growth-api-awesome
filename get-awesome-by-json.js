04b81dbf5fc3ef07aacc19eec16262fa27bdddbc
Fengda HUANG <h@phodal.com>
2017-05-15 22:07:50 +0800

    [T] update download scripts

--- get-awesome-by-json.js
+++ get-awesome-by-json.js
@@ -5,12 +5,30 @@ var request = require('request');
 var data = fs.readFileSync(__dirname + '/awesome-list.json', 'utf8');
 var AwesomeList = JSON.parse(data);
 
-_.each(AwesomeList, function(awesomeListByCategory){
-    _.each(awesomeListByCategory.childrens, function(awesome){
-        var url = 'https://raw.githubusercontent.com/' + awesome.github + '/master/README.md';
+_.each(AwesomeList, function (awesomeListByCategory) {
+    _.each(awesomeListByCategory.childrens, function (awesome) {
+        var baseUrl = 'https://raw.githubusercontent.com/' + awesome.github + '/master/';
+        var url = baseUrl + 'README.md';
         var path = 'download/' + awesome.slug + '.md';
 
-        console.log(url, path);
-        request(url).pipe(fs.createWriteStream(path))
+        request(url)
+            .on('error', function (err) {
+                // console.log(url, path);
+            })
+            .on('response', function (response) {
+                if(response.statusCode !== 200) {
+                    console.log(response.statusCode, path);
+                }
+                if(response.statusCode === 404){
+                    request(baseUrl + 'readme.md')
+                        .on('error', function (err) {
+                            console.log(err, path)
+                        })
+                        .pipe(fs.createWriteStream(path))
+
+
+                }
+            })
+            .pipe(fs.createWriteStream(path))
     });
 });
